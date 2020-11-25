04c61e379fdaf8f5fd710bd1a6b4afeaa010beb5
danielshamsudin <daniel.shamsudin@outlook.com>
2020-11-25 23:03:27 +0800

    added pictures in events

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -3,6 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendar = new FullCalendar.Calendar(calendarEl, {
         eventDidMount: function(info)
         {
+            if (-1 != info.event.title.indexOf("Node.js"))
+            {
+                info.el.style.color = 'grey';
+            }
+            if (-1 != info.event.title.indexOf("Flutter"))
+            {
+                info.el.style.color = 'green';
+            }
+            if (-1 != info.event.title.indexOf("Guest Lecture"))
+            {
+                info.el.style.color = 'brown';
+            }
             var s = moment(info.event.start).format("HH:mm");
             var e = moment(info.event.end).format("HH:mm");
             $(info.el).tooltip({
